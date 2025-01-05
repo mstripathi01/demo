177b2fcf3be375dcc29ba64d6a7a21f64faf10c8
mstripathi01 <mayank.tripathi218@gmail.com>
2025-01-05 19:14:39 +0530

    changes in app file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
+import logo from "./logo.svg";
 import "./App.css";
-import Data from "./data";
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello This, is Mayank Tripathi</h3>
-      <p>
-        The phrase "lorem ipsum" is a well-known typographical filler text used
-        in graphic design, printing, and digital publishing. It is a Latin
-        phrase that translates to "something about nothing" or "nonsense." Lorem
-        ipsum is often used to create visually appealing layouts, designs, and
-        mockups by providing placeholder text that resembles the appearance of
-        real text without the need for actual content. This allows designers to
-        focus on the layout and aesthetics without worrying about the text
-        itself. The use of lorem ipsum dates back to the 16th century.
-      </p>
-      <Data />
+      <h4>
+        Both are super useful for working with objects when you need to process
+        either the keys or values separately! 😊
+      </h4>
+      <p>Hello this is mayank</p>
     </div>
   );
 }
